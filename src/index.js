badfdce21fced79becc6a2b5eb5f9ed95ea53c23
Konstantin <konstkam@mail.ru>
2021-04-12 21:57:40 +0300

    Lesson 35. Deleted render.js, observer, subscribe

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import reportWebVitals from './reportWebVitals';
-import { rerenderEntireTree } from './render';
-import state from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import state, { addPost, updateNewPostText, updateNewMessageText, sendMessage, subscribe } from './redux/state';
 
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        state={state}
+        addPost={addPost}
+        updateNewPostText={updateNewPostText}
+        sendMessage={sendMessage}
+        updateNewMessageText={updateNewMessageText} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 reportWebVitals();
